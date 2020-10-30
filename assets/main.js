213bbcd84da3d191638b218a220b6253c6296c13
Ale <alessandrochb@gmail.com>
2020-10-30 15:38:30 +0100

    aggiunto document ready

--- assets/main.js
+++ assets/main.js
@@ -5,85 +5,92 @@ creare un array di oggetti "studente". Ciclare su tutti gli studenti e stampare 
 tramite 3 prompt, chiedere all'utente il nome, il cognome e l'età di un nuovo studente. Creare con questi dati un nuovo oggetto e inserirlo nell'array del punto precedente.
 */
 
-
-var student = {
-    'name' : 'pippo',
-    'surname' : 'verdi',
-    'age' : '20'
-};
-
-console.log(student);
-// console.log(student.name);
-// console.log(student['surname']);
-// console.log(student['age']);
-
-for (var key in student) {
-    // console.log(key);
-    // console.log(student[key]);
-    console.log(key + ' : ' + student[key]);
-}
+$(document).ready(function(){
 
 
-var students = [
-    {
+    var student = {
         'name' : 'pippo',
         'surname' : 'verdi',
         'age' : '20'
-    },
-    {
-        'name' : 'ciccio',
-        'surname' : 'bianchi',
-        'age' : '21'
-    },
-    {
-        'name' : 'mimmo',
-        'surname' : 'rossi',
-        'age' : '22'
-    },
-];
+    };
 
-//console.log(students.length);
+    console.log(student);
+    // console.log(student.name);
+    // console.log(student['surname']);
+    // console.log(student['age']);
 
-// questo è un array con all'interno degli oggetti
-// per ciclare l'array ci va un ciclo for
-// per ciclare gli oggetti ci va un ciclo for in
+    for (var key in student) {
+        // console.log(key);
+        // console.log(student[key]);
+        console.log(key + ' : ' + student[key]);
+    }
 
 
-for (var i = 0; i < students.length; i++) {
-    var elementoCorrente = students[i];
-    // console.log(students[i]);
+    var students = [
+        {
+            'name' : 'pippo',
+            'surname' : 'verdi',
+            'age' : '20'
+        },
+        {
+            'name' : 'ciccio',
+            'surname' : 'bianchi',
+            'age' : '21'
+        },
+        {
+            'name' : 'mimmo',
+            'surname' : 'rossi',
+            'age' : '22'
+        },
+    ];
 
-    for (var key in elementoCorrente) {
+    //console.log(students.length);
 
-        if (key == 'name' || key == 'surname') {
-            console.log('studente ' + (i + 1) + ' ' + key + ' : ' + elementoCorrente[key]);
+    // questo è un array con all'interno degli oggetti
+    // per ciclare l'array ci va un ciclo for
+    // per ciclare gli oggetti ci va un ciclo for in
+
+
+    for (var i = 0; i < students.length; i++) {
+        var elementoCorrente = students[i];
+        // console.log(students[i]);
+
+        for (var key in elementoCorrente) {
+
+            if (key == 'name' || key == 'surname') {
+                console.log('studente ' + (i + 1) + ' ' + key + ' : ' + elementoCorrente[key]);
+
+            }
         }
     }
-}
 
-var newStudent = {
-    'name' : prompt('inserisci il tuo nome'),
-    'surname' : prompt('inserisci il tuo cognome'),
-    'age' : parseInt(prompt('inserisci la tua età')),
-};
+    var newStudent = {
+        'name' : prompt('inserisci il tuo nome'),
+        'surname' : prompt('inserisci il tuo cognome'),
+        'age' : parseInt(prompt('inserisci la tua età')),
+    };
 
-console.log(newStudent);
+    console.log(newStudent);
 
-students.push(newStudent);
-console.log(students);
+    students.push(newStudent);
+    console.log(students);
 
 
-for (var i = 0; i < students.length; i++) {
-    var elementoCorrente = students[i];
-    // console.log(students[i]);
+    for (var i = 0; i < students.length; i++) {
+        var elementoCorrente = students[i];
+        // console.log(students[i]);
 
-    for (var key in elementoCorrente) {
+        for (var key in elementoCorrente) {
 
-        if (key == 'name' || key == 'surname') {
-            console.log('studente ' + (i + 1) + ' ' + key + ' : ' + elementoCorrente[key]);
+            if (key == 'name' || key == 'surname') {
+                console.log('studente ' + (i + 1) + ' ' + key + ' : ' + elementoCorrente[key]);
+            }
         }
     }
-}
+
+
+});
+
 
 
 
